c9a4ed1d887bfc55b658e9a4ca9eca6b03197087
sam.jheng <m3gl4a@gmail.com>
2019-05-24 23:57:19 +0800

    [style]-製作履歷第一頁

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Git page';
+summary = `  我叫宇佋是一名前端工程師，畢業於樹德科技大學資訊工程系，擁有一年以上專注於前端
+開發的經驗，對前端程式編成充滿熱情，想要把研究新的技術及架構應用在程式上，並喜歡
+與人合作，互相學習。`;
 }
